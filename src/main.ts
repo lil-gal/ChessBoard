e76197d0fbec8d352ae499716f8a104fb9c25e32
SmolPotato <toast.worthy.person@gmail.com>
2026-09-21 12:42:15 +0200

    Finished (forgot to 'add .', whoopsie)

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
 // npx tsc src/main.ts --outDir dist --watch 
+
+const chessboard = document.querySelector("#chessBoard")
+
+const whiteClassName = "white"
+const blackClassName = "black"
+
+const size = 8
+
+if(chessboard) {
+    for(let i = 0; i < size; i++){
+        for(let j = 0; j < size; j++){
+            const square = document.createElement("div")
+            if(i%2 == j%2){ // shoutout to Replicube for teaching me this!!
+                square.classList.add(whiteClassName)
+            }else{
+                square.classList.add(blackClassName)
+            }
+            chessboard.appendChild(square)
+        }
+        
+    }
+
+} 
